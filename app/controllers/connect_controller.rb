b6ef216082e6105a755beb1cdbb6e8a1ce6e5f12
Cally Gatehouse <Cally.Gatehouse@gmail.com>
2015-07-03 13:46:38 +0100

    better mac getting

--- app/controllers/connect_controller.rb
+++ app/controllers/connect_controller.rb
@@ -2,7 +2,10 @@ class ConnectController < ApplicationController
   
   def index
   	@ip = request.ip
-  	@mac = system('arp #{@ip}')
+  	#@mac = system('arp ')
+  	arp = %x[arp -a #{@ip}]
+  	@mac = arp.match('/..:..:..:..:..:../')[0]
+
   end
 
 

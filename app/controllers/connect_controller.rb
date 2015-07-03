3ba559f01c4e7048465828ad5fd06ffeb005c0a9
Cally Gatehouse <Cally.Gatehouse@gmail.com>
2015-07-03 14:22:03 +0100

    iptables attempt no 1

--- app/controllers/connect_controller.rb
+++ app/controllers/connect_controller.rb
@@ -2,15 +2,31 @@ class ConnectController < ApplicationController
   
   def index
   	@ip = request.ip
-  	#@mac = system('arp ')
   	arpreturn = %x[arp -a #{@ip}]
   	if arpreturn
   		@mac = arpreturn.match('..:..:..:..:..:..')
   	else
   		@mac = 'Something went wrong'
   	end
+  end
+
+
+  def create
+  	@ip = request.ip
+  	arpreturn = %x[arp -a #{@ip}]
+    if arpreturn
+  		@mac = arpreturn.match('..:..:..:..:..:..')
+  		@connect = %x[sudo iptables -I internet 1 -t mangle -m mac --mac-source #{@mac} -j RETURN ] 
+  	else
+  		redirect '/info'
+  	end  	
+  
+  end
+
+  def show
 
   end
+  
 
 
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,4 +5,7 @@ class StaticPagesController < ApplicationController
   def info
   end
 
+  def oops
+  end
+
 end
